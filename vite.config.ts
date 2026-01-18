0d61e38ce3a29fa7a0bcb6ec1ed474f4de1aaa98
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Air.local>
2026-01-18 15:20:29 -0500

    i mispelled the repo name lol

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fantasy-hockey-planner/',
+  base: '/fanasy-hockey-planner/',
   plugins: [react()],
   resolve: {
     alias: {
